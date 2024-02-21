6e0cc5a3b2d581f8d1eb8887336bbbf3e10bc846
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-21 18:51:44 +0300

    Added a project - json like string contains some graphics elements with attrs like x, y, color, width, etc

--- server/src/app.py
+++ server/src/app.py
@@ -5,6 +5,8 @@ Alexander Tyamin.
 This file contains the FastAPI application instance.
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
+
+project = '''{ "elements":[ { "id":1, "attributes":{ "name":"Element 1", "x":0, "y":0, "width":100, "height":100 } }, { "id":1, "attributes":{ "name":"Element 1", "x":0, "y":0, "width":100, "height":100 } } ] }'''
