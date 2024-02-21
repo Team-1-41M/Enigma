edf882ba69914c0c3cf8bc63f9488220a5559d82
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-21 18:56:26 +0300

    Test process route to check can several clients work with one project

--- server/src/app.py
+++ server/src/app.py
@@ -10,3 +10,29 @@ from fastapi import FastAPI, WebSocket
 app = FastAPI()
 
 project = '''{ "elements":[ { "id":1, "attributes":{ "name":"Element 1", "x":0, "y":0, "width":100, "height":100 } }, { "id":1, "attributes":{ "name":"Element 1", "x":0, "y":0, "width":100, "height":100 } } ] }'''
+
+clients = set()
+
+
+@app.websocket("/project")
+async def process(socket: WebSocket) -> None:
+    """
+    Collects project status changes from the client
+    and notifies other clients about this.
+
+    :param socket: a WebSocket object from FastAPI.
+    :return: None.
+    """
+    global project
+
+    await socket.accept()
+    clients.add(socket)
+
+    await socket.send_text(project)
+
+    while True:
+        project = await socket.receive_text()
+
+        for client in clients:
+            if client != socket:
+                await client.send_text(project)
