abee795cea6d500b94c79ec1964897bcb0ce894c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 19:21:39 +0300

    BASE_PATH in sys path

--- server/src/run.py
+++ server/src/run.py
@@ -7,7 +7,13 @@ Alexander Tyamin.
 Server launch script (entry point).
 """
 
+import sys
+
 import uvicorn
 
+from core.settings import BASE_PATH
+
 if __name__ == '__main__':
+    sys.path.append(BASE_PATH)
+
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
