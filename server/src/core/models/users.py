0bd206e3e3d891d3497959a14eff56518dae8ca7
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 13:12:55 +0300

    User by email method

--- server/src/core/models/users.py
+++ server/src/core/models/users.py
@@ -6,7 +6,10 @@ User model.
 """
 
 import datetime
+from typing import Optional
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
 
 from server.src.core.models.entity import Entity
@@ -22,3 +25,12 @@ class User(Entity):
     password: Mapped[str]
     is_active: Mapped[bool]
     login_at: Mapped[datetime.datetime] = mapped_column(server_default=None, nullable=True)
+
+    @staticmethod
+    async def by_email(
+            email: str,
+            session: AsyncSession,
+    ) -> Optional:
+        """Finds a user by email."""
+
+        return await session.scalar(select(User).where(User.email == email))
