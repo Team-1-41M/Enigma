a88e87bc9056f4cfd0a52c7a85fe5a937e2f5445
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 13:33:14 +0300

    Sign up route

--- server/src/api/v1/endpoints/auth.py
+++ server/src/api/v1/endpoints/auth.py
@@ -1,26 +1,69 @@
-import datetime
 import uuid
+import datetime
 from typing import Optional
 
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
-from starlette.exceptions import HTTPException
+from fastapi import APIRouter, Depends
+from passlib.context import CryptContext
 from starlette.responses import JSONResponse
+from starlette.exceptions import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from server.src.api.v1.schemas.users import UserSignInSchema
+from server.src.core.utils.db import get_db
 from server.src.core.models.users import User
-from server.src.core.settings import AUTH_ROUTER_PREFIX, SIGN_UP_URL, SIGN_IN_URL, SIGN_OUT_URL, ME_URL, SESSION_TTL
+from server.src.core.utils.crypt import crypt_context
 from server.src.core.utils.auth import authenticate_user
 from server.src.core.utils.cache import get_cache_storage
-from server.src.core.utils.db import get_db
+from server.src.api.v1.schemas.users import UserSignInSchema, UserSignUpSchema
+from server.src.core.settings import AUTH_ROUTER_PREFIX, SIGN_UP_URL, SIGN_IN_URL, SIGN_OUT_URL, ME_URL, SESSION_TTL
 
 router = APIRouter(prefix=AUTH_ROUTER_PREFIX)
 
 
 @router.post(SIGN_UP_URL)
-async def sign_up():
-    pass
+async def sign_up(
+        data: UserSignUpSchema,
+        db: AsyncSession = Depends(get_db),
+        cache_storage=Depends(get_cache_storage),
+        context: CryptContext = Depends(crypt_context),
+):
+    """
+    User account creation.
+    Login immediately.
+    """
+
+    same_email_user = await User.by_email(data.email, db)
+    same_name_user = await User.by_name(data.account_name, db)
+
+    if same_email_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with the same email address already exists"
+        )
+
+    if same_name_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with the same name already exists"
+        )
+
+    user = User(
+        name=data.name,
+        email=data.email,
+        password=context.hash(data.password),
+        is_active=True,
+    )
+
+    _ = await User.create(user, db)
+
+    return await sign_in(
+        UserSignInSchema(
+            account_name=data.account_name,
+            password=data.password
+        ),
+        db,
+        cache_storage
+    )
 
 
 @router.post(SIGN_IN_URL)
