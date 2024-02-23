0c9171a0e49b43559b8f276d8db50a4e6216dea4
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 13:38:55 +0300

    get_crypt_context func

--- server/src/core/utils/crypt.py
+++ server/src/core/utils/crypt.py
@@ -8,3 +8,9 @@ Crypt context for hashing passwords.
 from passlib.context import CryptContext
 
 crypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+async def get_crypt_context():
+    """Provides a crypt context for secure operations."""
+
+    return crypt_context
