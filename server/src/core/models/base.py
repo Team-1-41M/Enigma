a83e82682c2e0d4da7a9b8c0e341354290e72524
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 11:38:42 +0300

    Base model

---
+++ server/src/core/models/base.py
@@ -0,0 +1,14 @@
+"""
+23.02.2024
+Alexander Tyamin.
+
+Contains the base SQLAlchemy class for all models.
+"""
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs
+
+
+class Base(DeclarativeBase, AsyncAttrs):
+    """Base class for all models."""
+    pass
