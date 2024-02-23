3e9e191b144aa786df4952fa9a2e3e80c0e20924
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 13:26:08 +0300

    Sign in method

--- server/src/api/v1/endpoints/auth.py
+++ server/src/api/v1/endpoints/auth.py
@@ -1,6 +1,19 @@
-from fastapi import APIRouter
+import datetime
+import uuid
+from typing import Optional
 
-from server.src.core.settings import AUTH_ROUTER_PREFIX, SIGN_UP_URL, SIGN_IN_URL, SIGN_OUT_URL, ME_URL
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+from starlette.exceptions import HTTPException
+from starlette.responses import JSONResponse
+
+from server.src.api.v1.schemas.users import UserSignInSchema
+from server.src.core.models.users import User
+from server.src.core.settings import AUTH_ROUTER_PREFIX, SIGN_UP_URL, SIGN_IN_URL, SIGN_OUT_URL, ME_URL, SESSION_TTL
+from server.src.core.utils.auth import authenticate_user
+from server.src.core.utils.cache import get_cache_storage
+from server.src.core.utils.db import get_db
 
 router = APIRouter(prefix=AUTH_ROUTER_PREFIX)
 
@@ -11,8 +24,35 @@ async def sign_up():
 
 
 @router.post(SIGN_IN_URL)
-async def sign_in():
-    pass
+async def sign_in(
+        data: UserSignInSchema,
+        db: AsyncSession = Depends(get_db),
+        cache_storage=Depends(get_cache_storage),
+) -> JSONResponse:
+    """User authentication into the system with setting the session valueю."""
+
+    user: Optional[User] = await authenticate_user(data.name, data.password, db)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect name or password"
+        )
+
+    session_id = str(uuid.uuid4())
+    await cache_storage.set(session_id, user.id)
+
+    # TODO: later for Redis storage
+    # await session_storage.expire(session_id, SESSION_TTL)
+
+    response = JSONResponse({"detail": "Logged in successfully"})
+    response.set_cookie("session", session_id, max_age=SESSION_TTL)
+
+    updated_login_time = {
+        "login_at": datetime.datetime.now()
+    }
+    await user.update(updated_login_time, db)
+
+    return response
 
 
 @router.post(SIGN_OUT_URL)
