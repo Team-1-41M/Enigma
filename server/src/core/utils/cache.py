fb42ef0b4a9f2a89e2a8cf4c43625a85c34c0ea2
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 18:18:07 +0300

    CacheStorage class

--- server/src/core/utils/cache.py
+++ server/src/core/utils/cache.py
@@ -5,6 +5,24 @@ Alexander Tyamin.
 Storage for processing cache, user sessions, and any key-value data.
 """
 
+from abc import ABC, abstractmethod
+
+
+class CacheStorage(ABC):
+    """Provides a key-value storage."""
+
+    @abstractmethod
+    async def get(self, key):
+        pass
+
+    @abstractmethod
+    async def set(self, key, value):
+        pass
+
+    @abstractmethod
+    async def delete(self, key):
+        pass
+
 
 async def get_cache_storage():
     """Provides a key-value storage."""
