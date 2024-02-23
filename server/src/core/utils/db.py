ecd826c33c2a3411c0e1c21ab7aeeb7a695acd2a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 12:05:00 +0300

    Trying to get db connection

---
+++ server/src/core/utils/db.py
@@ -0,0 +1,29 @@
+"""
+23.02.2024
+Alexander Tyamin.
+
+Connecting to a database and providing a DB instance.
+"""
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncEngine
+
+engine: AsyncEngine = create_async_engine("sqlite+aiosqlite:///enigma.sqlite3")
+
+
+async def get_db() -> AsyncSession:
+    """
+    Provides a database session.
+    :return: AsyncSession
+    """
+
+    session_maker = async_sessionmaker(
+        bind=engine,
+        expire_on_commit=False,
+        autoflush=False
+    )
+
+    async with session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
