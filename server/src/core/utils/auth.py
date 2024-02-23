bd530bfd60d5f177bcc88d06f28e98c4b18942d4
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 13:16:45 +0300

    Authenticate user func

--- server/src/core/utils/auth.py
+++ server/src/core/utils/auth.py
@@ -4,14 +4,19 @@ Alexander Tyamin.
 
 Helper functions for working for users identification.
 """
+from typing import Optional
 
 from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from server.src.core.models.users import User
+from server.src.core.utils.crypt import crypt_context
 
 
 async def verify_password(
         plain_password: str,
         hashed_password: str,
-        crypt_context: CryptContext,
+        context: CryptContext = crypt_context,
 ) -> bool:
     """
     Verify hashed password.
@@ -19,9 +24,23 @@ async def verify_password(
     params:
         plain_password: raw user password
         hashed_password: hashed password stored in the database
-        crypt_context: helper with hashing algorithms
+        context: helper with hashing algorithms
 
     :return bool: True if passwords match, False otherwise.
     """
 
-    return crypt_context.verify(plain_password, hashed_password)
+    return context.verify(plain_password, hashed_password)
+
+
+async def authenticate_user(
+        name: str,
+        password: str,
+        db: AsyncSession,
+        context: CryptContext = crypt_context,
+) -> Optional[User]:
+    """Trying to find a user with same name and password."""
+
+    user = await User.by_email(name, db)
+    if user and await verify_password(password, user.password, context):
+        return user
+    return None
