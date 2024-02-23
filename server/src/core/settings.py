21c6c2e01adf0f70ac1e7c06064a31cc47477b09
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-23 11:32:10 +0300

    Some auth urls: sign* and me

--- server/src/core/settings.py
+++ server/src/core/settings.py
@@ -2,3 +2,8 @@ API_PREFIX = '/api'
 API_VERSION_1_PREFIX = API_PREFIX + '/v1'
 
 AUTH_ROUTER_PREFIX = '/auth'
+
+ME_URL = '/me'
+SIGN_UP_URL = '/sign-up'
+SIGN_IN_URL = '/sign-in'
+SIGN_OUT_URL = '/sign-out'
