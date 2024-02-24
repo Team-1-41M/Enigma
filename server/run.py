098128db07a31705afdeb9d88e2c1a6f3235bb3a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-24 11:25:07 +0300

    Using config value to set host, port and reload

--- server/run.py
+++ server/run.py
@@ -9,5 +9,12 @@ Server launch script (entry point).
 
 import uvicorn
 
+from server.root.settings import CONFIG
+
 if __name__ == '__main__':
-    uvicorn.run("root.asgi:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "root.asgi:app",
+        host=CONFIG["HOST"],
+        port=int(CONFIG["PORT"]),
+        reload=bool(CONFIG["RELOAD"]),
+    )
