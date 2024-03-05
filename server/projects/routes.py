58ff2640581bcfbdca690e350e38df8dab8d3021
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-05 09:34:43 +0300

    Added route to get a project by id

--- server/projects/routes.py
+++ server/projects/routes.py
@@ -15,6 +15,21 @@ async def items(db: AsyncSession = Depends(get_db)) -> dict:
     }
 
 
+@router.get('/')
+async def items(db: AsyncSession = Depends(get_db)) -> dict:
+    return {
+        "data": [_.dict() async for _ in Project.every(db)]
+    }
+
+
+@router.get('/{item_id}/')
+async def item(
+        item_id: int,
+        db: AsyncSession=Depends(get_db)
+):
+    return await Project.by_id(item_id, db)
+
+
 @router.post('/', response_model = ProjectDB)
 async def create_project(
         project: ProjectCreate,
