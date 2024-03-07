431eaec38cc855929d351ca8e83cb1467f661acc
KirillSemyonkin <burnytc@gmail.com>
2024-03-07 13:34:54 +0300

    Add User Display to Header

--- client/src/actions/auth.ts
+++ client/src/actions/auth.ts
@@ -1,18 +1,16 @@
-import type { AxiosResponse } from "axios";
-
-export const signUp = async (data: { name: string, password: string, email: string }): Promise<AxiosResponse<void, any>> => { // TODO proper error type
+export const signUp = async (data: { name: string, password: string, email: string }): Promise<void> => { // TODO proper error type
     const { $api } = useNuxtApp();
 
     return await $api.post('/api/v1/auth/sign-up', data);
 }
 
-export const signIn = async (data: { name: string, password: string }): Promise<AxiosResponse<void, any>> => { // TODO proper error type
+export const signIn = async (data: { name: string, password: string }): Promise<void> => { // TODO proper error type
     const { $api } = useNuxtApp();
 
     return await $api.post('/api/v1/auth/sign-in', data);
 }
 
-export const signOut = async (): Promise<AxiosResponse<void, any>> => { // TODO proper error type
+export const signOut = async (): Promise<any> => { // TODO proper error type
     const { $api } = useNuxtApp();
 
     return await $api.post('/api/v1/auth/sign-out');
--- client/src/actions/users.ts
+++ client/src/actions/users.ts
@@ -1,8 +1,7 @@
-import type { AxiosResponse } from "axios";
 import type { User } from "~/types/user";
 
-export const me = async (): Promise<AxiosResponse<User, any>> => { // TODO proper error type
+export const me = async (): Promise<User> => { // TODO proper error type
     const { $api } = useNuxtApp();
-
-    return await $api.post('/api/v1/auth/me'); // TODO /users/
+ 
+    return (await $api.get('/api/v1/auth/me')).data; // TODO /users/
 }
