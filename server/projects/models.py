f7dfecdb7a2d4f37e30463a4e57aef4fd86660d5
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-08 18:49:58 +0300

    Docstring for Project model

--- server/projects/models.py
+++ server/projects/models.py
@@ -11,6 +11,11 @@ from server.shared.models import Entity
 
 
 class Project(Entity):
+    """
+    Application interface layout/prototype 
+    project created on the platform by the user.
+    """
+
     __tablename__ = "projects"
 
     title: Mapped[str]
