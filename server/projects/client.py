39539d58676e713c46cf36ab72390c505f6c7a0b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-09 14:52:09 +0300

    Returned test client for websocket

---
+++ server/projects/client.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from aiohttp import ClientSession
+
+
+async def main():
+    async with ClientSession() as session:
+        async with session.ws_connect('http://127.0.0.1:8000/api/v1/projects/1/content') as ws:
+            loop = asyncio.get_event_loop()
+
+            async def send_data():
+                while True:
+                    data = await loop.run_in_executor(None, input, "")
+                    await ws.send_str(data)
+
+            async def receive_data():
+                while True:
+                    msg = await ws.receive_str()
+                    print(msg)
+
+            await asyncio.gather(send_data(), receive_data())
+
+
+asyncio.run(main())
