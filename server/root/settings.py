e2a4ebe7c7a4a02d028d8358be96326c95cbaab2
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-14 15:24:55 +0300

    Base path in settings

--- server/root/settings.py
+++ server/root/settings.py
@@ -5,4 +5,8 @@ Alexander Tyamin.
 Constants for the project, shared for different apps.
 """
 
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent.parent.parent
+
 SESSION_TTL = 3 * 24 * 60 * 60
