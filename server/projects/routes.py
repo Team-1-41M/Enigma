ef3764cf4014647458eb37902b57a9c9047f86ce
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-20 20:51:41 +0300

    Removed trailing whitespace

--- server/projects/routes.py
+++ server/projects/routes.py
@@ -243,7 +243,7 @@ async def process(
 
             else:
                 raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, 
+                    status_code=status.HTTP_400_BAD_REQUEST,
                     detail="Invalid command.",
                 )
 
