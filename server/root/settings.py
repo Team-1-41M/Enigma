4f6a26d1b0bb0ec24c323bc372824c52287aee90
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-20 13:01:18 +0300

    Path for different envs

--- server/root/settings.py
+++ server/root/settings.py
@@ -8,5 +8,6 @@ Constants for the project, shared for different apps.
 from pathlib import Path
 
 BASE_PATH = Path(__file__).resolve().parent.parent.parent
+ENVS_PATH = BASE_PATH / "envs"
 
 SESSION_TTL = 3 * 24 * 60 * 60
