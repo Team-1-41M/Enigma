25e0faf1c2d7a59761c5785c3b512e72b27978c1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-22 23:47:00 +0300

    In run - simplification of logic of loading envs

--- server/run.py
+++ server/run.py
@@ -14,23 +14,10 @@ from pathlib import Path
 import uvicorn
 from dotenv import load_dotenv
 
+load_dotenv()
+
 if __name__ == "__main__":
     sys.path.append(str(Path(__file__).resolve().parent.parent))
-
-    from root.settings import ENVS_PATH
-
-    load_dotenv(ENVS_PATH / ".debug")
-
-    if os.getenv("DEBUG") == "True":
-        ENV_PATH = "local"
-    else:
-        load_dotenv()
-        ENV_PATH = "docker"
-
-    RUNNING_ENV_PATH = ENVS_PATH / ENV_PATH
-
-    load_dotenv(RUNNING_ENV_PATH / ".db")
-    load_dotenv(RUNNING_ENV_PATH / ".server")
 
     uvicorn.run(
         "root.asgi:app",
