fb7b6e7ca97ab533d930e7cc7ed0b4d08f70f66c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-24 22:22:48 +0300

    Trying to add a test for sign in

--- server/auth/tests.py
+++ server/auth/tests.py
@@ -1,2 +1,31 @@
-def test_example():
-    assert True
+import os
+
+from fastapi.testclient import TestClient
+from starlette import status
+
+from run import app
+
+client = TestClient(app)
+
+
+def test_sign_in_normal() -> None:
+    """
+    Test normal sign in process with correct credentials.
+
+    Args:
+        None.
+
+    Returns:
+        None.
+    """
+
+    response = client.post(
+        "api/v1/auth/sign-in",
+        json={
+            "name": os.getenv("SUPERUSER_NAME"),
+            "password": os.getenv("SUPERUSER_PASSWORD"),
+        },
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.cookies["session"]
