13d9b16628f42ddeec4d5e8348ff437f13f2c2d6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-24 22:22:32 +0300

    Import app from asgi for pytest in run script

--- server/run.py
+++ server/run.py
@@ -16,11 +16,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-if __name__ == "__main__":
-    sys.path.append(str(Path(__file__).resolve().parent.parent))
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
+from server.root.asgi import app
+
+if __name__ == "__main__":
     uvicorn.run(
-        "root.asgi:app",
+        app,
         host=os.getenv("SERVER_HOST"),
         port=int(os.getenv("SERVER_PORT")),
         reload=bool(os.getenv("SERVER_RELOAD")),
