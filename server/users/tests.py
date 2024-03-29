c9656b8066029b8092533298ddc561e16c2cdcec
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-29 15:50:30 +0300

    So, trying to write some simple test to "me" route

--- server/users/tests.py
+++ server/users/tests.py
@@ -1,2 +1,10 @@
-def test_example():
-    assert True
+from fastapi.testclient import TestClient
+
+from server.root.asgi import app
+
+client = TestClient(app)
+
+
+def test_me_session_not_provided():
+    response = client.get("/api/v1/users/me")
+    assert response.status_code == 401
