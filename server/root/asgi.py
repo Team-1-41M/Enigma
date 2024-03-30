824f1824f8c2b3be6aa1d3e1e9945b4c1228b2ae
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-03-30 12:54:44 +0300

    Fixed deprecation warning with start up event. Used lifespan

--- server/root/asgi.py
+++ server/root/asgi.py
@@ -1,15 +1,27 @@
 import os
 from pathlib import Path
+from typing import AsyncGenerator
 
 from fastapi import APIRouter, FastAPI
+from contextlib import asynccontextmanager
 from server.auth.routes import router as auth_router
 from server.projects.routes import router as projects_router
 from server.root.db import init_db
 from server.users.routes import router as users_router
 from starlette.staticfiles import StaticFiles
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:
+    """Run operations on application startup and shutdown."""
+    
+    await init_db()
+
+    yield
+
+
 debug = os.getenv("DEBUG") == "True"
-app = FastAPI(debug=debug)
+app = FastAPI(debug=debug, lifespan=lifespan)
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 MEDIA_DIR = BASE_DIR / "media"
@@ -25,10 +37,3 @@ api_v1_router.include_router(users_router)
 api_v1_router.include_router(projects_router)
 
 app.include_router(api_v1_router)
-
-
-@app.on_event("startup")
-async def startup_event() -> None:
-    """Run db initialization."""
-    
-    await init_db()
