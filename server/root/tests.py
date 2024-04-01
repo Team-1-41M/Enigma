0392c7d2de45c50ce8378ebce06a677a7847b208
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-04-01 19:10:33 +0300

    Tests for password verifying

--- server/root/tests.py
+++ server/root/tests.py
@@ -1,3 +1,6 @@
+import pytest
+from server.root.auth import verify_password
+from server.root.crypt import get_crypt_context
 from server.root.db import build_url
 
 
@@ -56,3 +59,32 @@ def test_build_db_url_no_credentials_and_location():
         "engine": "engine",
         "name": "name",
     }) == "engine:///name"
+
+
+@pytest.mark.asyncio
+async def test_verify_password_and_hash_equal():
+    """Test: compare the hash of one password with the hash of same password."""
+
+    context = await get_crypt_context()
+
+    plain_password = "password"
+    hashed_password = context.hash(plain_password)
+
+    verified: bool = await verify_password(plain_password, hashed_password, context)
+
+    assert verified
+
+
+@pytest.mark.asyncio
+async def test_verify_password_and_hash_not_equal():
+    """Test: compare the hash of one password with the hash of another password."""
+
+    context = await get_crypt_context()
+
+    plain_password = "password"
+    wrong_password = "wrongpassword"
+    hashed_password = context.hash(wrong_password)
+
+    verified: bool = await verify_password(plain_password, hashed_password, context)
+
+    assert not verified
