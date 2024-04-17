01aa26be35d91be6c041a90432df6fcf37c12c1a
Stenyushkin D. <19722@gapps.ispu.ru>
2024-04-17 17:12:21 +0300

    Added schemas for join and commit

--- server/projects/schemas.py
+++ server/projects/schemas.py
@@ -1,4 +1,5 @@
 from typing import Literal
+from datetime import datetime
 
 from pydantic import BaseModel
 from server.shared.schemas import EntityDBSchema
@@ -8,7 +9,6 @@ class ProjectBaseSchema(BaseModel):
     """Minimal information about the project."""
 
     title: str
-
 
 class ProjectCreateSchema(ProjectBaseSchema):
     """Information that the user must provide to create a project."""
@@ -34,6 +34,30 @@ class ProjectItemsSchema(BaseModel):
 
     length: int
     data: list[ProjectDBSchema]
+
+
+class JoinCreateSchema(BaseModel):
+    """Information to join another user to a project."""
+
+    user_id: int
+
+class JoinDBSchema(JoinCreateSchema, EntityDBSchema):
+    """Join data in the database"""
+
+    project_id: int
+
+class CommentCreate(BaseModel):
+    text: str
+
+class CommentOut(BaseModel):
+    id: int
+    project_id: int
+    user_id: int
+    text: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
 
 
 class AccessSchema(BaseModel):
